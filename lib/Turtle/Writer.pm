2d4d4c7dbaa1c58d987f10802028f5e491f9608b
Jakob Voß <jakob.voss@gbv.de>
2012-07-04 12:46:43 +0000

    Import of VOJ/Turtle-Writer-0.002 from CPAN.
    gitpan-cpan-distribution: Turtle-Writer
    gitpan-cpan-version:      0.002
    gitpan-cpan-path:         VOJ/Turtle-Writer-0.002.tar.gz
    gitpan-cpan-author:       VOJ
    gitpan-cpan-maturity:     released

--- lib/Turtle/Writer.pm
+++ lib/Turtle/Writer.pm
@@ -1,38 +1,12 @@
 package Turtle::Writer;
 {
-  $Turtle::Writer::VERSION = '0.001';
+  $Turtle::Writer::VERSION = '0.002';
 }
 #ABSTRACT: Write RDF/Turtle documents without non-core package dependencies
 
 use strict;
 use warnings;
 
-=head1 SYNOPSIS
-
-  use Turtle::Writer;
-
-  say turtle_statement( 
-    "<$uri>",
-      "a" => "<http://purl.org/ontology/bibo/Document>",
-      "dc:creator" => { # plain literals are escaped
-	  	"" => [ "Terry Winograd", "Fernando Flores" ]
-      },
-      "dc:date" => { "xs:gYear" => "1987" }, # typed literal
-      "dc:title" =>
-          { en => "Understanding Computers and Cognition" },
-      "dc:description" => undef,  # will be ignored
-  );
-
-=head1 DESCRIPTION
-
-Turtle::Writer is a lightweight helper module for Perl programs that write RDF
-data in RDF/Turtle syntax. For this purpose the module exports four methods:
-C<turtle_statement>, C<turtle_literal>, C<turtle_literal_list>, and
-C<turtle_uri>. This methods may be handy to directly create serialized RDF from
-other forms of structured data.  For a full featured RDF/Trine serializer see
-L<RDF::Trine::Serializer::Turtle>.
-
-=cut
 
 use Carp;
 use Scalar::Util qw(reftype);
@@ -40,14 +14,6 @@ use base 'Exporter';
 
 our @EXPORT = qw(turtle_literal turtle_literal_list turtle_statement turtle_uri);
 
-=method turtle_statement ( $subject, $predicate => $object [, ... ] )
-
-Returns a (set of) RDF statements in Turtle syntax. Subject and predicate
-parameters must be strings. Object parameters must either be strings or
-arrays of strings. This function strips undefined values and empty strings,
-but it does not further check or validate parameter values.
-
-=cut
 
 sub turtle_statement {
     my ($subject, %statements) = @_;
@@ -72,14 +38,6 @@ sub turtle_statement {
     return "$subject " . join(" ;\n" , shift @s, map { "    $_" } @s) . " .\n";
 }
 
-=method turtle_literal ( $string [ [ lang => ] $lang | [ type => ] $datatype ] )
-
-Returns a literal string escaped in Turtle syntax. You can optionally provide
-either a language or a full datatype URI (but their values are not validated).
-Returns the empty string instead of a Turtle value, if C<$string> is C<undef>
-or the empty string.
-
-=cut
 
 sub turtle_literal {
     my $value = shift;
@@ -116,11 +74,6 @@ sub turtle_literal {
     return $value;
 }
 
-=method turtle_literal_list ( $literal | @array_of_literals | { $language => $literal } )
-
-Returns a list of literal strings in Turtle syntax.
-
-=cut
 
 sub turtle_literal_list {
     if ( ref($_[0]) and ref($_[0]) eq 'HASH') {
@@ -136,13 +89,6 @@ sub turtle_literal_list {
     }
 }
 
-=method turtle_uri ( $uri )
-
-Returns an URI in Turtle syntax, that is C<< "<$uri>" >>. Returns the 
-empty string, if C<$uri> is C<undef>, but C<< <> >> if C<$uri> is the
-empty string. In most cases you better directly write C<< "<$uri>" >>.
-
-=cut
 
 sub turtle_uri {
     my $value = shift;
@@ -152,3 +98,83 @@ sub turtle_uri {
 }
 
 1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Turtle::Writer - Write RDF/Turtle documents without non-core package dependencies
+
+=head1 VERSION
+
+version 0.002
+
+=head1 SYNOPSIS
+
+  use Turtle::Writer;
+
+  say turtle_statement( 
+    "<$uri>",
+      "a" => "<http://purl.org/ontology/bibo/Document>",
+      "dc:creator" => { # plain literals are escaped
+	  	"" => [ "Terry Winograd", "Fernando Flores" ]
+      },
+      "dc:date" => { "xs:gYear" => "1987" }, # typed literal
+      "dc:title" =>
+          { en => "Understanding Computers and Cognition" },
+      "dc:description" => undef,  # will be ignored
+  );
+
+=head1 DESCRIPTION
+
+Turtle::Writer is a lightweight helper module for Perl programs that write RDF
+data in Turtle syntax. No non-core packages are required.  Before directly
+writing RDF/Turtle by hand, have a look at this module.  Before using this
+module, have a look at L<RDF::Trine::Serializer::Turtle> which provides a full
+featured serializer for RDF data in Turtle syntax.
+
+By default this module exports four methods: C<turtle_statement>,
+C<turtle_literal>, C<turtle_literal_list>, and C<turtle_uri>. This methods may
+be handy to directly create serialized RDF from other forms of structured data.
+Literal values are escaped and C<undef> is ignored, among other features.
+
+=head1 METHODS
+
+=head2 turtle_statement ( $subject, $predicate => $object [, ... ] )
+
+Returns a (set of) RDF statements in Turtle syntax. Subject and predicate
+parameters must be strings. Object parameters must either be strings or
+arrays of strings. This function strips undefined values and empty strings,
+but it does not further check or validate parameter values.
+
+=head2 turtle_literal ( $string [ [ lang => ] $lang | [ type => ] $datatype ] )
+
+Returns a literal string escaped in Turtle syntax. You can optionally provide
+either a language or a full datatype URI (but their values are not validated).
+Returns the empty string instead of a Turtle value, if C<$string> is C<undef>
+or the empty string.
+
+=head2 turtle_literal_list ( $literal | @array_of_literals | { $language => $literal } )
+
+Returns a list of literal strings in Turtle syntax.
+
+=head2 turtle_uri ( $uri )
+
+Returns an URI in Turtle syntax, that is C<< "<$uri>" >>. Returns the 
+empty string, if C<$uri> is C<undef>, but C<< <> >> if C<$uri> is the
+empty string. In most cases you better directly write C<< "<$uri>" >>.
+
+=head1 AUTHOR
+
+Jakob Voss
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Jakob Voss.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
